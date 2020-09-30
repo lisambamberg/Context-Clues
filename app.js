63ab4671d13c8367eb39964e4af6cbd4fa9a37bd
lisambamberg <lisambamberg@gmail.com>
2020-09-30 13:59:07 -0500

    Editing loop

--- app.js
+++ app.js
@@ -7,10 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
     div.className = "newDiv";
     squareContainer.appendChild(div);
 
-    for (var i = 1; i < 101; i++) {
-        var h3 = document.createElement("h3");
-        div.appendChild(h3);
-        var text = document.createTextNode(`Accusation ${[i]}`);
-        h3.appendChild(text);
+    function makeFunc() {
+        var friendArray = ["Professor Plum", "Colonel Mustard", "Mrs. White", "Mrs. Peacock", "Mr. Green"];
+        var locationArray = ["lounge", "ballroom", "study", "kitchen", "hall", "dining room", "conversatory", "library", "billard room", "cellar"];
+        var weaponArray = ["candlestick", "knife", "lead pipe", "revolver", "rope", "wrench", "lamp", "letter-opener", "cigarette holder", "pool cue", "wine bottle", "antique microscope", "Ming vase", "an original Renoir painting", "rotary phone", "miniture obelisk", "monocle chain", "afghan woven rug", "sherry decanter", "opera glasses"];
+
+        function displayH3() {
+            for (var i = 1; i < 101; i++) {
+                var h3 = document.createElement("h3");
+                div.appendChild(h3);
+                var text = document.createTextNode(`Accusation ${[i]}`);
+                h3.appendChild(text);
+                var alertText = alertText()
+                h3.addEventListener("click", function () {
+                    alertText;
+                })
+            }
+        }
+        function alertText() {
+            return function () {
+                alert(`I accuse ${friendArray} with the ${weaponArray} in the ${locationArray}!`)
+            }
+        }
     }
 });
